7c352e3050a9d36d4829e94e3cd2a062876ad438
syasin <syasinahya@gmail.com>
2018-07-10 17:10:26 +0900

    なんかうごく

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import copy
 from numeron import Numeron, Answer
 
-from itertools import permutations 
 def main(level):
     numeron = Numeron(level)
     answer = Answer("".join([str(i) for i in range(level)]))
+    numeron.post_answer(answer)
     answer_before = copy.deepcopy(answer)
     """
     blow 特定パート
     """
-    while(answer.blows + answer.hits is numeron.level):
+    while(answer.blows + answer.hits is not numeron.level):
         for i in range(numeron.level):
             for ans_i in range(10):
                 answer_before.set_used()
@@ -18,17 +18,6 @@ def main(level):
                 else:
                     answer_before.answer = change_str(answer.answer, i, ans_i)
                     numeron.post_answer(answer_before)
-                    # answerを変えていって、blowsが減った結果、元々使われてた数字がblow対象.
-                    if answer_before.blows < answer.blows:
-                        answer.what_is_blow[int(answer.answer[i])] = True
-                        answer_before.answer = answer.answer
-                        break
-                    # 逆にanswerを変えていって、blowsが増えた結果、変えた数字がblow対象.
-                    elif answer_before.blows > answer.blows:
-                        answer.what_is_blow[int(answer_before.answer[i])] = True
-                        answer.answer = answer_before.answer
-                        answer.blows = answer_before.blows
-                        break
                     # hitsに対しても同様
                     if answer_before.hits < answer.hits:
                         answer.what_is_hit[i] = True
@@ -39,25 +28,33 @@ def main(level):
                         answer.answer = answer_before.answer
                         answer.hits = answer_before.hits
                         break
+                    # answerを変えていって、blowsが減った結果、元々使われてた数字がblow対象.
+                    elif answer_before.blows < answer.blows:
+                        answer.what_is_blow[int(answer.answer[i])] = True
+                        answer_before.answer = answer.answer
+                        break
+                    # 逆にanswerを変えていって、blowsが増えた結果、変えた数字がblow対象.
+                    elif answer_before.blows > answer.blows:
+                        answer.what_is_blow[int(answer_before.answer[i])] = True
+                        answer.answer = answer_before.answer
+                        answer.blows = answer_before.blows
+                        break
             if answer.blows + answer.hits is numeron.level:
                 break
     print("end of blows")
-    print(answer.what_is_hit)
+
     """
     hit 特定パート
     """
     answer_post = copy.deepcopy(answer)
-    answer_post2 = copy.deepcopy(answer)
+
     while(1):
         # i桁目とj桁目を交換していく
         for i in range(numeron.level):
             # 確定したi桁目は除外
-            print(i)
             if answer.what_is_hit[i]:
                 continue
             for j in range(i+1,numeron.level):
-                print(i,j)
-                print(answer.what_is_hit)
                 # 確定したj桁目は除外
                 if answer.what_is_hit[j]:
                     continue
@@ -75,7 +72,7 @@ def main(level):
                     elif answer_post.hits - answer.hits is 1:
                         answer.answer = answer_post.answer
                         answer.hits = answer_post.hits
-                        break
+                        break            
     print("end")
         
 def change_str(string, i , change):
